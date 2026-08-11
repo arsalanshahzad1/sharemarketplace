422aff72d271c558ab7c3bb192b4d474dde91f1a
Muhammad Ahmer Khan <ahmer.pluton@gmail.com>
2026-08-11 22:40:46 +0500

    fix : enabled preview config in vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,18 @@ export default defineConfig({
       port: 5173,
     },
   },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 5100,
+    allowedHosts: [
+      'marketplace.javatimescaffe.com',
+      '159.223.114.165',
+    ],
+  },
+
   resolve: {
     alias: {
-      // Absolute imports: `@/components/...` instead of `../../../components/...`
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
